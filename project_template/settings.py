0c2c3ac958aee9060ff1f45166aae9eb678d03d1
Emmanuel C <capelle.e@gmail.com>
2019-11-15 17:02:57 +0100

    AutoPush

--- project_template/settings.py
+++ project_template/settings.py
@@ -36,6 +36,7 @@ ALLOWED_HOSTS = [] if is_debug == 1 else [
 # Application definition
 
 INSTALLED_APPS = [
+    'django_extensions',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
