ec5b258bb13bff0983dda37b773dd31d77e94751
Emmanuel C <capelle.e@gmail.com>
2019-11-15 15:55:54 +0100

    initial commit

---
+++ venv-project-template/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/emm/projects/blastm3dia/libraries-and-templates/django/project_template/venv-project-template/bin/python3.7
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
